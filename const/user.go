b2038c3d49708fe3b7d37d525f9529012e1e3973
rigon <2412789408@qq.com>
2025-10-20 09:47:58 +0800

    refactor: add user const

---
+++ const/user.go
@@ -0,0 +1,13 @@
+package _const
+
+const (
+	UserPassswdMustMoreSix = "密码长度不能小于6位"
+	UserTokenParseFailed   = "token解析失败"
+	UserNotFound           = "用户未找到"
+	UserNotActive          = "用户未激活"
+)
+
+const (
+	UserNameLengthMin = 3
+	UserNameLenghtMax = 20
+)
